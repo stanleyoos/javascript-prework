5919e24f7d3b4d9d922162371b88d2d0ed65c1cd
stanleyos <stanislaw.sarka@gmail.com>
2022-12-01 15:55:28 +0100

    Added some logic: computer and user moves

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-let playerMove = 'papier'
+let playerInput = prompt('Wybierz swój ruch: 1.kamień 2.papier 3.nożyce')
+let playerMove = 'nieznany ruch'
+
+if (playerInput == 1) {
+  playerMove = 'kamień'
+} else if (playerInput == 2) {
+  playerMove = 'papier'
+} else if (playerInput == 3) {
+  playerMove = 'nożyce'
+}
+
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+console.log('Wylosowana liczba to: ' + randomNumber)
+
+let computerMove = 'nieznany ruch'
+
+if (randomNumber == 1) {
+  computerMove = 'kamień'
+} else if (randomNumber == 2) {
+  computerMove = 'papier'
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce'
+}
